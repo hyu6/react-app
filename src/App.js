7f42eb74e19b374018d618251e65761ec7805eed
Haoran Yu <haoranyu6@gmail.com>
2020-01-18 12:13:05 -0800

    Add an event handler

--- src/App.js
+++ src/App.js
@@ -3,14 +3,45 @@ import "./App.css";
 import Person from "./Person/Person";
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: "A", age: 28 },
+      { name: "B", age: 16 },
+      { name: "C", age: 30 }
+    ]
+  };
+
+  switchNameHandler = () => {
+    // console.log("Clicked!");
+    // DON'T DO THIS: this.state.persons[0].name = "Switch";
+    this.setState({
+      persons: [
+        { name: "Switch", age: 28 },
+        { name: "B", age: 16 },
+        { name: "C", age: 30 }
+      ]
+    });
+  };
+
   render() {
     return (
       <div className="App">
         <h1>React</h1>
-        <Person name="A" age="28" />
-        <Person name="B" age="17">
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+        >
           Text
         </Person>
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+        />
       </div>
     );
   }
