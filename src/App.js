85350ba1cc9e1805bfcc1fd793861ce75e12882f
Haoran Yu <haoranyu6@gmail.com>
2020-02-11 12:05:28 -0800

    Handle dynamic content and output lists

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ class App extends Component {
       { name: "B", age: 16 },
       { name: "C", age: 30 }
     ],
-    otherState: "Other state"
+    otherState: "Other state",
+    showPersons: false
   };
 
   switchNameHandler = newName => {
@@ -34,6 +35,11 @@ class App extends Component {
     });
   };
 
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({ showPersons: !doesShow });
+  };
+
   render() {
     const style = {
       backgroundColor: "white",
@@ -43,28 +49,25 @@ class App extends Component {
       cursor: "pointer"
     };
 
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          {this.state.persons.map(person => {
+            return <Person name={person.name} age={person.age} />;
+          })}
+        </div>
+      );
+    }
+
     return (
       <div className="App">
         <h1>React</h1>
-        <button style={style} onClick={() => this.switchNameHandler("Switch")}>
+        <button style={style} onClick={this.togglePersonsHandler}>
           Switch Name
         </button>
-        <Person
-          name={this.state.persons[0].name}
-          age={this.state.persons[0].age}
-        />
-        <Person
-          name={this.state.persons[1].name}
-          age={this.state.persons[1].age}
-          click={this.switchNameHandler.bind(this, "Person")}
-          changed={this.nameChangedHandler}
-        >
-          Text
-        </Person>
-        <Person
-          name={this.state.persons[2].name}
-          age={this.state.persons[2].age}
-        />
+        {persons}
       </div>
     );
   }
